1e54e2d6244e059374e5a03778aab4fcf09d28b8
abihu <max_data@163.com>
2017-02-04 08:58:11 +0800

    创建站点管理员对象

--- jcms-system-admin/src/main/java/org/jcms/system/admin/entity/Manager.java
+++ jcms-system-admin/src/main/java/org/jcms/system/admin/entity/Manager.java
@@ -3,6 +3,9 @@
  */
 package org.jcms.system.admin.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -18,8 +21,248 @@ import javax.persistence.Table;
 @Table(name="t_manager")
 public class Manager {
 	@Id
-	@Column
+	@Column(name="id")
 	private int id;
+	
+	private String userName;
+	private String nickName;
+	private String password;
+	private String createTime;
+	private String roleIds;
+	private String creator;		//创建者名字
+	private int creatorId;		//创建者编号
+	
+	/**
+	 * 
+	 */
+	public Manager() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @param id
+	 * @param userName
+	 * @param nickName
+	 * @param password
+	 * @param createTime
+	 * @param roleIds
+	 * @param creator
+	 * @param creatorId
+	 */
+	public Manager(int id, String userName, String nickName, String password,
+			String createTime, String roleIds, String creator, int creatorId) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.nickName = nickName;
+		this.password = password;
+		this.createTime = createTime;
+		this.roleIds = roleIds;
+		this.creator = creator;
+		this.creatorId = creatorId;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((createTime == null) ? 0 : createTime.hashCode());
+		result = prime * result + ((creator == null) ? 0 : creator.hashCode());
+		result = prime * result + creatorId;
+		result = prime * result + id;
+		result = prime * result
+				+ ((nickName == null) ? 0 : nickName.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((roleIds == null) ? 0 : roleIds.hashCode());
+		result = prime * result
+				+ ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Manager other = (Manager) obj;
+		if (createTime == null) {
+			if (other.createTime != null)
+				return false;
+		} else if (!createTime.equals(other.createTime))
+			return false;
+		if (creator == null) {
+			if (other.creator != null)
+				return false;
+		} else if (!creator.equals(other.creator))
+			return false;
+		if (creatorId != other.creatorId)
+			return false;
+		if (id != other.id)
+			return false;
+		if (nickName == null) {
+			if (other.nickName != null)
+				return false;
+		} else if (!nickName.equals(other.nickName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (roleIds == null) {
+			if (other.roleIds != null)
+				return false;
+		} else if (!roleIds.equals(other.roleIds))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Manager [id=" + id + ", userName=" + userName + ", nickName="
+				+ nickName + ", password=" + password + ", createTime="
+				+ createTime + ", roleIds=" + roleIds + ", creator=" + creator
+				+ ", creatorId=" + creatorId + "]";
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the nickName
+	 */
+	public String getNickName() {
+		return nickName;
+	}
+	/**
+	 * @param nickName the nickName to set
+	 */
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the createTime
+	 */
+	public String getCreateTime() {
+		return createTime;
+	}
+	/**
+	 * @param createTime the createTime to set
+	 */
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	/**
+	 * @return the roleIds
+	 */
+	public String getRoleIds() {
+		return roleIds;
+	}
+	/**
+	 * @param roleIds the roleIds to set
+	 */
+	public void setRoleIds(String roleIds) {
+		this.roleIds = roleIds;
+	}
+	
+	public void setRoleIdsStr(Set<Integer>roleIds){
+		String rIds = "";
+		for(int rid:roleIds){
+			rIds += rid+",";
+		}
+		int i = rIds.lastIndexOf(",");
+		rIds = rIds.substring(0, i);
+		this.roleIds = rIds;
+	}
+	
+	public Set<Integer> getIntRoleIds(){
+		Set<Integer> roleIds = new HashSet<Integer>();
+		String[] rIds = this.roleIds.split(",");
+		for(String id:rIds){
+			roleIds.add(Integer.parseInt(id));
+		}
+		return roleIds;
+	}
+	
+	/**
+	 * @return the creator
+	 */
+	public String getCreator() {
+		return creator;
+	}
+	/**
+	 * @param creator the creator to set
+	 */
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	/**
+	 * @return the creatorId
+	 */
+	public int getCreatorId() {
+		return creatorId;
+	}
+	/**
+	 * @param creatorId the creatorId to set
+	 */
+	public void setCreatorId(int creatorId) {
+		this.creatorId = creatorId;
+	}
+	
 	
 	
 
