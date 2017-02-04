3f54d8926479f88c1ea60cca5bd6f35e2008943d
ramostear <ramostear@163.com>
2017-02-04 15:44:12 +0800

    创建ManagerDao接口

--- jcms-system-admin/src/main/java/org/jcms/system/admin/dao/ManagerDao.java
+++ jcms-system-admin/src/main/java/org/jcms/system/admin/dao/ManagerDao.java
@@ -3,11 +3,14 @@
  */
 package org.jcms.system.admin.dao;
 
+import org.jcms.base.dao.BaseDao;
+import org.jcms.system.admin.entity.Manager;
+
 /**
  * @Author Abihu[谭朝红] - - -2017年2月3日-下午5:11:52
  * @Info http://www.abihu.org
  * @Description:
  */
-public interface ManagerDao {
+public interface ManagerDao extends BaseDao<Manager, Integer>{
 
 }
