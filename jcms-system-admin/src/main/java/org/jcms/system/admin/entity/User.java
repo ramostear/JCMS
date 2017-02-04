2fea7c7bf9dec79b8cf3b2ab9ec2f22b3950c360
ramostear <ramostear@163.com>
2017-02-04 15:50:09 +0800

    创建用户对象

---
+++ jcms-system-admin/src/main/java/org/jcms/system/admin/entity/User.java
@@ -0,0 +1,269 @@
+/**
+ *	Copyright [2017] [www.ramostear.com]
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");<br/>
+ *	you may not use this file except in compliance with the License.<br/>
+ *	You may obtain a copy of the License at<br/>
+ *							<br/>
+ *	    http://www.apache.org/licenses/LICENSE-2.0<br/>
+ *							<br/>
+ *	Unless required by applicable law or agreed to in writing, software<br/>
+ *	distributed under the License is distributed on an "AS IS" BASIS,<br/>
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.<br/>
+ *	See the License for the specific language governing permissions and<br/>
+ *	limitations under the License.<br/>
+ * 
+ */
+package org.jcms.system.admin.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @Author Abihu[谭朝红] - - -2017年2月4日-下午3:45:58
+ * @Info http://www.abihu.org
+ * @Description:
+ */
+@Entity
+@Table(name="t_user")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	private String userName;
+	private String nickName;
+	private String password;
+	private String email;
+	private String tel;
+	private String registryTime;
+	private String address;
+	
+	/**
+	 * 
+	 */
+	public User() {
+	}
+
+	/**
+	 * @param id
+	 * @param userName
+	 * @param nickName
+	 * @param password
+	 * @param email
+	 * @param tel
+	 * @param registryTime
+	 * @param address
+	 */
+	public User(int id, String userName, String nickName, String password,
+			String email, String tel, String registryTime, String address) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.nickName = nickName;
+		this.password = password;
+		this.email = email;
+		this.tel = tel;
+		this.registryTime = registryTime;
+		this.address = address;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the nickName
+	 */
+	public String getNickName() {
+		return nickName;
+	}
+
+	/**
+	 * @param nickName the nickName to set
+	 */
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the tel
+	 */
+	public String getTel() {
+		return tel;
+	}
+
+	/**
+	 * @param tel the tel to set
+	 */
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	/**
+	 * @return the registryTime
+	 */
+	public String getRegistryTime() {
+		return registryTime;
+	}
+
+	/**
+	 * @param registryTime the registryTime to set
+	 */
+	public void setRegistryTime(String registryTime) {
+		this.registryTime = registryTime;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + id;
+		result = prime * result
+				+ ((nickName == null) ? 0 : nickName.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((registryTime == null) ? 0 : registryTime.hashCode());
+		result = prime * result + ((tel == null) ? 0 : tel.hashCode());
+		result = prime * result
+				+ ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nickName == null) {
+			if (other.nickName != null)
+				return false;
+		} else if (!nickName.equals(other.nickName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (registryTime == null) {
+			if (other.registryTime != null)
+				return false;
+		} else if (!registryTime.equals(other.registryTime))
+			return false;
+		if (tel == null) {
+			if (other.tel != null)
+				return false;
+		} else if (!tel.equals(other.tel))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", nickName="
+				+ nickName + ", password=" + password + ", email=" + email
+				+ ", tel=" + tel + ", registryTime=" + registryTime
+				+ ", address=" + address + "]";
+	}
+}
