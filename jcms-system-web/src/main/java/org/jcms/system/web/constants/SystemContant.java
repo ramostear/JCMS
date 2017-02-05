1f52b7bfbedac425a1ba37d8e81b66a2da2af983
ramostear <ramostear@163.com>
2017-02-06 04:08:03 +0800

    创建系统全局常量

---
+++ jcms-system-web/src/main/java/org/jcms/system/web/constants/SystemContant.java
@@ -0,0 +1,28 @@
+/**
+ *	Copyright [2017] [www.ramostear.com]
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");<br/>
+ *	you may not use this file except in compliance with the License.<br/>
+ *	You may obtain a copy of the License at<br/>
+ *							<br/>
+ *	    http://www.apache.org/licenses/LICENSE-2.0<br/>
+ *							<br/>
+ *	Unless required by applicable law or agreed to in writing, software<br/>
+ *	distributed under the License is distributed on an "AS IS" BASIS,<br/>
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.<br/>
+ *	See the License for the specific language governing permissions and<br/>
+ *	limitations under the License.<br/>
+ * 
+ */
+package org.jcms.system.web.constants;
+
+/**
+ * @Author Abihu[谭朝红] - - -2017年2月6日-上午3:47:14
+ * @Info http://www.abihu.org
+ * @Description:
+ */
+public class SystemContant {
+	
+	public static final String LOGIN_MANAGER = "loginManager";
+
+}
