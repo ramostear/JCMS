825c13529ac5d7e0df60ae6827bb25ec537f9561
ramostear <ramostear@163.com>
2017-02-07 09:50:48 +0800

    创建工具类模块，添加了验证码生成器类：VerifyCodeUtil

---
+++ jcms-system-utils/src/main/java/org/jcms/system/utils/QRCode.java
@@ -0,0 +1,28 @@
+/**
+ *	Copyright [2017] [www.ramostear.com]
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");<br/>
+ *	you may not use this file except in compliance with the License.<br/>
+ *	You may obtain a copy of the License at<br/>
+ *							<br/>
+ *	    http://www.apache.org/licenses/LICENSE-2.0<br/>
+ *							<br/>
+ *	Unless required by applicable law or agreed to in writing, software<br/>
+ *	distributed under the License is distributed on an "AS IS" BASIS,<br/>
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.<br/>
+ *	See the License for the specific language governing permissions and<br/>
+ *	limitations under the License.<br/>
+ * 
+ */
+package org.jcms.system.utils;
+
+/**
+ * @Author Abihu[谭朝红] - - -2017年2月7日-上午9:24:29
+ * @Info http://www.abihu.org
+ * @Description: 二维码生成工具类
+ */
+public class QRCode {
+	
+	
+
+}
